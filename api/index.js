e0b6d73fc2300eb8f539532a832787a04b5397c1
Sravanam charan <sravanamcharan20@gmail.com>
2024-04-01 23:14:14 +0530

    pre

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import path from 'path';
 
 dotenv.config();
 
@@ -14,7 +15,17 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 })
 
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+
 app.use(express.json());
 
 app.listen(3000,()=>{

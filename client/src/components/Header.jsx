6b84e4f14b8514386adaac9eae9bd076ca8a9ff2
Sravanam charan <sravanamcharan20@gmail.com>
2024-04-02 21:37:39 +0530

    header updated with privateRoute

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,29 @@
+
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div className='bg-slate-300'>
+    <div className='bg-slate-200'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold  text-2xl'>Auth App</h1>
+          <h1 className='font-bold'>Auth App</h1>
         </Link>
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='text-slate-700 text-lg hover:text-blue-700'>Home</li>
+            <li>Home</li>
           </Link>
           <Link to='/about'>
-            <li className='text-slate-700 text-lg hover:text-blue-700'>About</li>
+            <li>About</li>
           </Link>
-          <Link to='/sign-in'>
-            <li className='text-blue-500 text-lg hover:text-blue-700'>Sign In</li>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
-          
         </ul>
       </div>
     </div>
